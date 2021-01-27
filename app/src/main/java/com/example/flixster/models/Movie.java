efbfaa25f6c46482aa412cbb9a48bddfa92f20b5
Paul Leiva <54649136+paul-leiva@users.noreply.github.com>
2021-01-26 22:04:19 -0500

    added JSON parsing

--- app/src/main/java/com/example/flixster/models/Movie.java
+++ app/src/main/java/com/example/flixster/models/Movie.java
@@ -1,7 +1,11 @@
 package com.example.flixster.models;
 
+import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class Movie {
 
@@ -13,5 +17,25 @@ public class Movie {
         posterPath = jsonObject.getString("poster_path");
         title = jsonObject.getString("title");
         overview = jsonObject.getString("overview");
+    }
+
+    public static List<Movie> fromJsonArray(JSONArray moviejsonArray) throws JSONException {
+        List <Movie> movies = new ArrayList<>();
+        for (int i = 0; i < moviejsonArray.length(); i++) {
+            movies.add(new Movie(moviejsonArray.getJSONObject(i)));
+        }
+        return movies;
+    }
+
+    public String getPosterPath() {
+        return String.format("https://image.tmdb.org/t/p/w342/%s", posterPath);
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public String getOverview() {
+        return overview;
     }
 }
